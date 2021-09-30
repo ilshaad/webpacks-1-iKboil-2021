da03ecb519063fcec42c09b226b4180ace37bdee
ik <rechad.kheerdali@gmail.com>
2021-09-30 13:17:57 +0100

    included prettier code formatter

---
+++ iK.sh
@@ -0,0 +1,5 @@
+cd 01javascriptWebpackTemplate/ && npm run ikupdate && \
+cd ../02javascriptWebpackJestTestingTemplate/ && npm run ikupdate && \
+cd ../03reactWebpackTemplate/ && npm run ikupdate && \
+cd ../04reactWebpackJestReacttestinglibraryTestingTemplate/ && npm run ikupdate && \
+exit
